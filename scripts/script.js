864bfbaa4726054ce130515f482522e90d637920
Nicolas Radziwon <websiteshepa@gmail.com>
2024-07-09 00:01:17 -0300

    version_2.0

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,8 @@ const anillo2 = document.getElementById('anillo2');
 const likesVapo = document.getElementById('likesVapo');
 const likesEnElHall = document.getElementById('likesEnElHall');
 const container = document.getElementById('container');
+const patternVapo = document.getElementById('patternVapo');
+const patternEnElHall = document.getElementById('patternEnElHall');
 
 const URLapi = 'https://668c33570b61b8d23b0cc0e8.mockapi.io/metrica/likes';
 
@@ -28,7 +30,6 @@ function accionIzquierda() {
         resetSides();
         videosVistos();
     } else {
-        toggleOrientation();
         if (side !== 'left'){
             if (window.innerWidth > 1024){
                 leftSide.style.visibility = "visible";
@@ -38,24 +39,28 @@ function accionIzquierda() {
                 botonCircular.style.right = "0";
                 botonCircular.style.left = "90%";
             } else {
+                container.style.transform = "rotate(90deg)";
+                leftSide.style.removeProperty('display');
                 rightSide.style.display = "none";
+                container.style.width = "100vh";
+                container.style.height = "100vw";
+                botonCircular.style.right = "0";
+                botonCircular.style.left = "80%";
             }
+            patternVapo.style.display = "none";
+            patternEnElHall.style.display = "none";
             leftSideVideo.muted = false;
             leftSideVideo.volume = 0.3;
             leftSideVideo.load();
             rightSideVideo.muted = true;
             side = 'left';
             leftSide.classList.add("vistoLeft");
-            smokeFilter.style.visibility = "hidden";
             likesVapo.style.display = "block";
             likesEnElHall.style.display = "none";
         } else {
             resetSides(); 
-            smokeFilter.style.visibility = "visible";
             leftSideVideo.muted = true;
             rightSideVideo.muted = true;
-            leftSide.style.display = "block";
-            rightSide.style.display = "block";
         }
     }
 }
@@ -67,7 +72,6 @@ function accionDerecha() {
         resetSides();
         videosVistos();
     } else {
-        toggleOrientation();
         if (side !== 'right'){
             if (window.innerWidth > 1024){
                 rightSide.style.visibility = "visible";
@@ -77,22 +81,29 @@ function accionDerecha() {
                 botonCircular.style.left = "0";
                 botonCircular.style.right = "90%";
             } else {
-
+                container.style.transform = "rotate(90deg)";
+                container.style.width = "100vh";
+                container.style.height = "100vw";
+                rightSide.style.removeProperty('display');
+                leftSide.style.display = "none";
+                botonCircular.style.left = "0";
+                botonCircular.style.right = "80%";
             }
+            patternVapo.style.display = "none";
+            patternEnElHall.style.display = "none";
             rightSideVideo.muted = false;
             rightSideVideo.volume = 0.3;
             rightSideVideo.load();
             leftSideVideo.muted = true;
             side = 'right';
             rightSide.classList.add("vistoRight");
-            smokeFilter.style.visibility = "hidden";
             likesEnElHall.style.display = "block";
             likesVapo.style.display = "none";
         } else {
             resetSides(); 
-            smokeFilter.style.visibility = "visible";
             leftSideVideo.muted = true;
             rightSideVideo.muted = true;
+            
         }
     }
 }
@@ -101,11 +112,20 @@ function resetSides() {
     if (window.innerWidth > 1024){
         leftSide.style.flex = 1;
         rightSide.style.flex = 1;
-        botonCircular.style.left = "0";
-        botonCircular.style.right = "0";
         rightSide.style.visibility = "visible";
         leftSide.style.visibility = "visible";
+
+    } else {
+        leftSide.style.removeProperty('display');
+        rightSide.style.removeProperty('display');
+        container.style.transform = "rotate(0deg)";
+        container.style.width = "100vw";
+        container.style.height = "auto";
     }
+    patternVapo.style.display = "block";
+    patternEnElHall.style.display = "block";
+    botonCircular.style.left = "0";
+    botonCircular.style.right = "0";
     side = null;
     likesEnElHall.style.display = "none";
     likesVapo.style.display = "none";
@@ -116,22 +136,8 @@ function videosVistos() {
     botonIzq.style.backgroundColor = "transparent";
     leftSideVideo.muted = true;
     rightSideVideo.muted = true;
-    smokeFilter.style.visibility = "visible";
     botonIzq.innerHTML = "";
     botonDer.innerHTML = "";
-}
-
-function toggleOrientation() {
-    if (window.orientation === 0 || window.orientation === 180) {
-        debugger;
-        container.style.transform = "rotate(90deg)";
-        container.style.width = "100vh";
-        container.style.height = "100vw";
-    } else {
-        container.style.transform = "rotate(0deg)";
-        container.style.width = "100vw";
-        container.style.height = "100vh";
-    }
 }
 
 function likeVapo() {
